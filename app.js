3964930689ad6cd18b1c31e45947f9c9e67d48aa
Virgílio Cano <virgiliocano53@gmail.com>
2020-06-27 16:36:30 -0300

    Hear the sounds

--- app.js
+++ app.js
@@ -13,8 +13,27 @@ class DrumKit {
   }
   repeat() {
     let step = this.index % 8;
-    console.log(step);
     const activeBars = document.querySelectorAll(`.b${step}`);
+    //Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if pads are active
+      if (bar.classList.contains("active")) {
+        //check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
@@ -29,6 +48,9 @@ const drumkit = new DrumKit();
 
 drumkit.pads.forEach((pad) => {
   pad.addEventListener("click", drumkit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumkit.playBtn.addEventListener("click", function () {
